8c656e3acffd071250298f6c0eccda6be634d421
alexeyk500 <alexeyk500@yandex.ru>
2023-11-18 19:45:53 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { binarySearch } from './src/binaySearch.js';
+
+// 1. BinarySearch
+const values = [1,2,7,12,15,16,27,38,49,60,];
+const position = binarySearch(values, 12)
+console.log('position', position)
